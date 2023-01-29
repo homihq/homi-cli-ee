c879f9f8e4ef1092d25ce270a002e7fcf8c3a67a
grabdoc <grabdoc2020@gmail.com>
2023-01-29 17:30:11 -0600

    Initial commit

---
+++ src/main/java/com/homihq/shell/HomiCliEeApplication.java
@@ -0,0 +1,13 @@
+package com.homihq.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiCliEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomiCliEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homihq/shell/HomiCliEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homihq.shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomiCliEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
